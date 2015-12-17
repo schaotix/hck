4e8b132264e5a8339221259df52ec0a321b929e4
screamer <screamer.chaotix.24@gmail.com>
2015-12-16 19:55:55 -0500

    files from home

---
+++ size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("\n\n\t\t Some Sizes For Ya \n");
+	printf("\t\t---------------------------\n");
+	printf("\t\tThe size of int is: %d\n", sizeof(int));
+	printf("\t\tThe size of long is: %d\n", sizeof(long int));
+	printf("\t\tThe size of long long is: %d\n", sizeof(long long int));
+	printf("\t\tThe size of short int: %d\n", sizeof(short int));
+	printf("\t\tThe size of float is: %d\n", sizeof(float));
+	printf("\t\tThe size of char is: %d\n", sizeof(char));
+	printf("\t\tAnd the size of unsigned int is: %d\n\n", sizeof(unsigned int));
+}
